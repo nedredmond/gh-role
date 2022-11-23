d706ee60d8420939c9efb04c165764195e264210
Ned <23404711+nedredmond@users.noreply.github.com>
2022-11-22 22:33:44 -0500

    test evaluate

--- eval.go
+++ eval.go
@@ -2,48 +2,43 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
 	"strings"
 )
 
-func evaluate(entity string, userRole string, roles []string, friendly bool) {
+func Evaluate(entity string, userRole string, roles []string, friendly bool) error {
 	// If no roles were specified, print the role
 	if len(roles) == 0 {
-		succeed(entity, userRole, friendly)
+		Succeed(entity, userRole, friendly)
 	}
 
 	// Otherwise, check if the user has one of the specified roles
 	for _, role := range roles {
 		if strings.EqualFold(userRole, role) {
-			succeed(entity, userRole, friendly)
+			Succeed(entity, userRole, friendly)
 		}
 	}
 
 	// If we got here, the user doesn't have any of the specified roles
-	fail(entity, userRole, roles)
+	return Fail(entity, userRole, roles)
 }
 
-func succeed(entity string, userRole string, friendly bool) {
+func Succeed(entity string, userRole string, friendly bool) {
 	roleString := strings.ToLower(userRole)
 	if friendly {
 		fmt.Printf("User has %s role in %s.\n", roleString, entity)
 	} else {
 		fmt.Println(roleString)
 	}
-	os.Exit(0)
 }
 
-func fail(entity string, userRole string, checkedRoles []string) {
+func Fail(entity string, userRole string, checkedRoles []string) error {
 	s := ""
 	if len(checkedRoles) > 1 {
 		s = "s"
 	}
 
-	log.Fatal(
-		fmt.Errorf(
-			"user does not have role%s in %s: %s; found %s",
-			s, entity, strings.Join(checkedRoles, ", "), strings.ToLower(userRole),
-		),
+	return fmt.Errorf(
+		"user does not have role%s in %s: %s; found %s",
+		s, entity, strings.Join(checkedRoles, ", "), strings.ToLower(userRole),
 	)
 }
---
+++ eval_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEvaluate_fail_roles(t *testing.T) {
+	assert.ErrorContains(
+		t,
+		Evaluate("council", "jedi", []string{"master", "padawan"}, true),
+		"user does not have roles in council: master, padawan; found jedi",
+	)
+}
+
+func TestEvaluate_fail_role(t *testing.T) {
+	assert.ErrorContains(
+		t,
+		Evaluate("council", "jedi", []string{"master"}, false),
+		"user does not have role in council: master; found jedi",
+	)
+}
+
+func ExampleEvaluate_succeed_noroles_friendly() {
+	Evaluate("council", "jedi", []string{}, true)
+	// Output: User has jedi role in council.
+}
+
+func ExampleEvaluate_succeed_noroles_unfriendly() {
+	Evaluate("council", "jedi", []string{}, false)
+	// Output: jedi
+}
+
+func ExampleEvaluate_succeed_roles_friendly() {
+	Evaluate("council", "jedi", []string{"jedi"}, false)
+	// Output: jedi
+}
